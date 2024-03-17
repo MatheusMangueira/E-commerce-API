357a6ab3224e15bc16d336f4ec53a3f8e8bb66c7
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-03-17 14:11:30 -0300

    fea: connection with the bank

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { Server } from './server/Server';
+import { AppDataSource } from './server/config/database';
+
+
+
+AppDataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
 
 
 const server = new Server();
+
 server.listen(3000);
--- src/server/config/database/index.ts
+++ src/server/config/database/index.ts
@@ -1,1 +1,17 @@
-export { };
+import { DataSource } from 'typeorm';
+import { ProductModel } from '../../model';
+import 'dotenv/config';
+
+
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  entities: [ProductModel], 
+});
+
