e926a7f9e769d2250f215cfeb99dbd57ae0fb0c9
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-03-28 01:59:15 -0300

    feat: create service for orderItem

---
+++ src/server/shared/services/orderItem/OrderItemService.ts
@@ -0,0 +1,52 @@
+import { Repository } from 'typeorm';
+import { OrderItemDto } from '../../../DTOs/orderItemDto/OrderItemDto';
+import { OrderItemModel } from '../../../model/orderItem/orderItemModel ';
+import { OrderModel, ProductModel } from '../../../model';
+
+export class OrderItemService {
+  constructor(
+    private orderItemRepository: Repository<OrderItemModel>,
+    private orderRepository: Repository<OrderModel>,
+    private productRepository: Repository<ProductModel>
+  ) { }
+
+  async create(data: OrderItemDto) {
+  
+    try {
+      const product = await this.productRepository.findOne({
+        where: { id: data.product.id }
+      });
+
+      if (!product) {
+        throw new Error(`Product with ID ${ data.product.id} not found`);
+      }
+
+      const order = await this.orderRepository.findOne({
+        where: { id: data.order.id }
+      });
+
+      if (!order) {
+        throw new Error(`Order with ID ${data.order.id} not found`);
+      }
+
+      const orderItem = this.orderItemRepository.create({
+        productId: product,
+        order: order,
+        quantity: data.quantity
+      });
+
+      const createOrderItem = await this.orderItemRepository.save(orderItem);
+
+      return createOrderItem;
+
+    } catch (error) {
+      console.log(error, 'erro no service, create()');
+      throw new Error('Internal Server Error');
+    }
+
+
+
+  }
+
+
+}
