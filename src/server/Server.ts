4b2f6916f596fe6b3e0a8d138429c46203beac98
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-03-29 15:45:00 -0300

    feat: config for documentation (swagger)

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import { AllRouter } from './routes';
 import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const router = new AllRouter().getRouter();
 
 export const App = express();
 
+App.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 App.use(express.json());
 App.use(router);
 
